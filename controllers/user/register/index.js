fde1eb9ee0c2311457ef077ded6b7e01560e0aca
Bu Kinoshita <bukinoshita@gmail.com>
2019-02-23 17:24:49 -0300

    minor: add register user controller

---
+++ controllers/user/register/index.js
@@ -0,0 +1,26 @@
+// Helpers
+const findOneUser = require('../../../helpers/find-one-user')
+const createUser = require('../../../helpers/create-user')
+const sendVerifyAccountEmail = require('../../../helpers/send-verify-account-email')
+
+const register = async (req, res) => {
+  const { email } = req.body
+  let user = await findOneUser({ email })
+
+  if (user) {
+    await sendVerifyAccountEmail(user)
+    return res.status(200).send({ data: user })
+  }
+
+  try {
+    user = await createUser({ email })
+    await sendVerifyAccountEmail(user)
+
+    res.status(200).send({ data: user })
+  } catch (error) {
+    console.error({ error })
+    res.status(400).send({ error })
+  }
+}
+
+module.exports = register
