f8a9440ef1229daf2774cc8f078735939bcdab82
Bu Kinoshita <bukinoshita@gmail.com>
2019-02-23 15:05:52 -0300

    minor: add route

---
+++ routes/index.js
@@ -0,0 +1,13 @@
+// Package
+const { name, version, description } = require('./package')
+
+module.exports = app => {
+  app.get('/', (req, res) => {
+    res.send({
+      name,
+      version,
+      description,
+      environment: process.env.NODE_ENV
+    })
+  })
+}
