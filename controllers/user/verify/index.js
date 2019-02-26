93ffa1dc73de7a2d4c64ecd80332a4222cbd7cec
Bu Kinoshita <bukinoshita@gmail.com>
2019-02-25 21:24:38 -0300

    patch: fix update user token when verify

--- controllers/user/verify/index.js
+++ controllers/user/verify/index.js
@@ -1,19 +1,22 @@
 // Helpers
-const findOneToken = require('../../../helpers/find-one-token')
-const updateToken = require('../../../helpers/update-token')
+const findOneAndUpdateToken = require('../../../helpers/find-and-update-token')
 
-module.exports = async (req, res) => {
+const verify = async (req, res) => {
   const { user } = req
 
   if (user) {
-    const token = await findOneToken(user._id, { createdAt: -1 })
+    const token = await findOneAndUpdateToken(
+      { userId: user._id, used: true, expired: false },
+      { expired: true },
+      { sort: { createdAt: -1 }, new: true }
+    )
 
-    if (token && token.used && !token.expired) {
-      await updateToken(token, { expired: true })
-
+    if (token && token.used && token.expired) {
       return res.status(200).send({ data: { status: true } })
     }
   }
 
   res.status(200).send({ data: { status: false } })
 }
+
+module.exports = verify
