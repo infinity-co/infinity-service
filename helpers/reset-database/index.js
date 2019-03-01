d3e5fe1f1da7273a3161d68a3ea3f2dac76f5134
Bu Kinoshita <bukinoshita@gmail.com>
2019-03-01 14:32:18 -0300

    patch: add reset subscription collection

--- helpers/reset-database/index.js
+++ helpers/reset-database/index.js
@@ -3,6 +3,7 @@ const Promise = require('bluebird')
 
 // Models
 const UserModel = Promise.promisifyAll(require('../../models/user'))
+const SubscriptionModel = Promise.promisifyAll(require('../../models/subscription'))
 const TokenModel = Promise.promisifyAll(require('../../models/token'))
 
 const env = process.env.NODE_ENV || 'development'
@@ -10,7 +11,11 @@ const env = process.env.NODE_ENV || 'development'
 module.exports = () => {
   if (env === 'test') {
     return new Promise((resolve, reject) => {
-      Promise.all([UserModel.deleteMany({}), TokenModel.deleteMany({})])
+      Promise.all([
+        UserModel.deleteMany({}),
+        SubscriptionModel.deleteMany({}),
+        TokenModel.deleteMany({})
+      ])
         .then(() => resolve())
         .catch(error => reject(error))
     })
