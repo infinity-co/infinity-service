64120f6e4092caa92da7ec908b4573730c165131
Bu Kinoshita <bukinoshita@gmail.com>
2019-03-01 12:43:36 -0300

    patch: fix mailer service in test environment

--- services/mailer.js
+++ services/mailer.js
@@ -7,8 +7,14 @@ const { mailer } = require('../config')
 const { apiKey, domain, from } = mailer
 const mailgun = new Mailgun({ apiKey, domain })
 
+const env = process.env.NODE_ENV || 'development'
+
 const transporter = {
   sendMail: data => {
+    if (env === 'test') {
+      return console.info(`Email sent to ${data.to}`)
+    }
+
     data.from = from
 
     mailgun.messages().send(data, error => {
