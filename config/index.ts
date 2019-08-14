fe01cd1168ae1a2c1a06f9c36533999cd39f00d6
bukinoshita <bukinoshita@gmail.com>
2019-08-14 01:49:08 -0300

    feat: add database connection

--- config/index.ts
+++ config/index.ts
@@ -1,12 +1,8 @@
-module.exports = {
-  port: process.env.PORT,
-  database: process.env.DATABASE,
-  tokenSecret: process.env.TOKEN_SECRET,
-  host: process.env.HOST,
-  mailer: {
-    apiKey: process.env.MAILER_APIKEY,
-    domain: process.env.MAILER_DOMAIN,
-    from: process.env.MAILER_FROM
-  },
-  corsOrigin: process.env.CORS_ORIGIN
+export default {
+  database: {
+    username: process.env.DATABASE_USERNAME,
+    password: process.env.DATABASE_PASSWORD,
+    cluster: process.env.DATABASE_CLUSTER,
+    options: process.env.DATABASE_OPTIONS
+  }
 }
